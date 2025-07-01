07f861701db24d872f92316cce49f1ff812dc748
Leonardo Mota Meira Filho <leonardomotamfilho@gmail.com>
2025-06-30 22:54:09 -0300

    chore(App.tsx): Remoção de dependências não usadas

--- ru-frontend/src/App.tsx
+++ ru-frontend/src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import SideNavBar from './components/SideNavBar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import UserPages from './pages/UserPages/UserPages'
