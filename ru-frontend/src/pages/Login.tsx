4f7b3e54370d65148b2c9c346efb421f99bc2f46
Jose Ramon Severo Alves <jose.ramon@splab.ufcg.edu.br>
2025-07-11 23:40:07 -0300

    feat(telaLogin): cria primeiro esboco e implementa logica simples para o login

--- ru-frontend/src/pages/Login.tsx
+++ ru-frontend/src/pages/Login.tsx
@@ -1,3 +1,99 @@
-export default function Login(){
-    return(<div>Login</div>)
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login({
+  setLogged,
+}: {
+  logged: boolean;
+  setLogged: (logged: boolean) => void;
+}) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    console.log(email, password);
+    if (email === "admin" && password === "admin") {
+      setLogged(true);
+      navigate("/user/");
+    } else {
+      alert("Usuário ou senha inválidos");
+    }
+  };
+
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <>
+      <h1 className="text-6xl font-bold text-center m-20 text-indigo-900">SysAdminRU</h1>
+
+      <div className="flex items-center justify-center bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-md">
+          <h2 className="text-2xl font-bold text-center text-gray-900">
+            Login
+          </h2>
+          <div className="space-y-6 p-4 border border-gray-300 rounded-xl border-4">
+            <div>
+              <label
+                htmlFor="Email"
+                className="text-sm font-medium text-gray-700"
+              >
+                Email:
+              </label>
+              <input
+                id="Email"
+                name="Email"
+                type="text"
+                required
+                className="w-full px-3 py-2 mt-1 bg-gray-200 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                value={email}
+                onChange={onChangeEmail}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-700"
+              >
+                Senha:
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="w-full px-3 py-2 mt-1 bg-gray-200 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                value={password}
+                onChange={onChangePassword}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="w-full m-1 px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={handleSubmit}
+              >
+                Entrar funcionário
+              </button>
+              <button
+                type="submit"
+                className="w-full m-1 px-4 py-2 text-sm font-medium text-white bg-yellow-300 border border-transparent rounded-md shadow-sm hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                onClick={handleSubmit}
+              >
+                Entrar admin
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default Login;
