6faf71d6d34e4f3c5e50d5d5defb49b9055cd114
kevenalex <keven.alex.barbosa.santos@ccc.ufcg.edu.br>
2025-07-31 07:41:16 -0300

    feat: inicia implementacao do service de autenticacao

---
+++ ru-frontend/src/services/AuthService.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+class AuthService {
+  baseUrl = window._env_.REACT_APP_API_URL;
+  endpoint;
+
+  constructor() {
+    this.endpoint = 'authenticate';
+  }
+
+  authenticate(requestData = {}) {
+    return axios.post(`${this.baseUrl}/${this.endpoint}`, requestData);
+  }
+}
+
+const instance = new AuthService();
+
+export default instance;
