82e88b3dab6b275c17d9e4e6d5323568449e3071
Leonardo Mota Meira Filho <leonardomotamfilho@gmail.com>
2025-08-27 15:01:47 -0300

    feat(Auth): ao não possuir token ou token expirar, é enviado de volta a tela de login

--- ru-frontend/src/components/RemoveButton.tsx
+++ ru-frontend/src/components/RemoveButton.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function RemoveButton({ onClickFunction }: { onClickFunction: () => void }) {
+export default function RemoveButton({ onClickFunction, nomeEntidade }: { onClickFunction: () => void, nomeEntidade: String }) {
 
     const [clicked, setClicked] = useState(false);
 
@@ -20,7 +20,7 @@ export default function RemoveButton({ onClickFunction }: { onClickFunction: () 
                     <button type="button" onClick={onClickFunction} className="cursor-pointer bg-red-700 hover:bg-red-800 focus:ring-4 rounded-lg text-m text-white font-bold  px-5 py-2.5 me-2 mb-2">Confirmar</button>
                 </div>
                 :
-                <button type="button" onClick={showConfirmacao} className="cursor-pointer bg-red-700 hover:bg-red-800 focus:ring-4 rounded-lg text-m text-white font-bold  px-5 py-2.5 me-2 mb-2">Excluir Funcionário</button>
+                <button type="button" onClick={showConfirmacao} className="cursor-pointer bg-red-700 hover:bg-red-800 focus:ring-4 rounded-lg text-m text-white font-bold  px-5 py-2.5 me-2 mb-2">Desativar {nomeEntidade}</button>
             }
         </div>)
 
